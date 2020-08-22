e067e40d33693ae90dd84ae1ccf528277df3fe97
berkaydemirel <berkay_demirel22@hotmail.com>
2020-08-22 22:01:52 +0300

    initial commit

---
+++ basiccart/src/main/java/com/berkay22demirel/basiccart/BasicCartApplication.java
@@ -0,0 +1,13 @@
+package com.berkay22demirel.basiccart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCartApplication.class, args);
+	}
+
+}
